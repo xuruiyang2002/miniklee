4cf6fc292265ce2cf2bde71d579fdff2f4ed8952
xuruiyang2002 <xury5520@outlook.com>
2024-11-08 14:59:37 +0800

    fix: solve warning caused by using  cast_or_null before declaring

--- include/Ref.h
+++ include/Ref.h
@@ -4,6 +4,15 @@
 template<class T>
 class ref;
 
+#include "llvm/Support/Casting.h"
+
+using llvm::cast;
+using llvm::cast_or_null;
+using llvm::dyn_cast;
+using llvm::dyn_cast_or_null;
+using llvm::isa;
+using llvm::isa_and_nonnull;
+
 /// Reference counter to be used as part of a ref-managed struct or class
 class ReferenceCounter {
     template<class T>
