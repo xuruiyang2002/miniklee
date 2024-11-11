b6c367780722af20f121f65739589f6f3b507f52
xuruiyang2002 <xury5520@outlook.com>
2024-11-11 10:01:26 +0800

    test: add test case

--- test/loop.c
+++ test/loop.c
@@ -2,11 +2,6 @@ int main() {
     int a = 1;
     int b = 2;
     int c = a + b;
-    // if (c > 3) {
-    //     a = 3;
-    // } else {
-    //     a = 4;
-    // }
     for (int i = 0; i < 5; i++) {
         a = a + 2;
     }
