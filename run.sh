c3e2a82560d33193ad8d62afd09e83d01ee2b9c5
xuruiyang2002 <xury5520@outlook.com>
2024-11-14 20:34:13 +0800

    refactor: refine the script

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-make
+make clean && make
 
 # Set SRC and OUT from the command-line arguments or default values if not provided
-SRC=${1:-./test/constraint}
+SRC=${1:-./test/multi_branch}
 
 # Run make with the specified SRC and OUT values
 make run SRC="$SRC".c OUT="$SRC".ll
